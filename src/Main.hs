0c7d6896ffe3d4b0f51ca3b87d7e1cb188bddfd5
Benjamin Summers <ben@benjaminsummers.net>
2015-01-13 01:36:01 +0000

    Some random code to test graphing.

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,22 @@ module Main where
 
 type Proc = IO ()
 
+infixr 9 %%
+
+(%%) :: (a -> b) -> a -> b
+a %% b = a b
+
+identity :: a -> a
+identity = id
+
+dump :: String -> Proc
+dump = putStrLn
+
 hello :: Proc
-hello = putStrLn "hello world!"
+hello = identity %% dump %% identity %% "hello world!"
 
 main :: Proc
-main = hello
+main = do
+  foo <- hello
+  bar <- hello
+  hello
