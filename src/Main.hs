c17b532e699ca1b70d41951cadc143f062f7e107
Benjamin Summers <ben@benjaminsummers.net>
2015-01-17 21:45:23 +0000

    Unicode characters.

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,28 @@
+{-# LANGUAGE UnicodeSyntax #-}
+
 module Main where
 
-infixr 9 %%
+infixr 9 █
 
 type Proc = IO ()
-(%%) :: (a -> b) -> a -> b
-identity :: a -> a
-dump :: String -> Proc
-hello :: Proc
-main :: Proc
+(█) ∷ (a → b) → a → b
+identity ∷ a → a
+dump ∷ String → Proc
+hello ∷ Proc
+main ∷ Proc
 
-a %% b = a b
+a █ b = a b
 
 identity = id
 
 dump "" = putStrLn ""
 dump x  = putStrLn x
 
-hello = identity %% dump %% identity %% "hello world!"
+hello = identity █ dump █ identity █ "你好世界"
 
 main = do
-  foo <- hello
-  bar <- hello
+  foo ← hello
+  bar ← hello
   let dump = undefined
-  (%%) <- return $ undefined dump
+  (█) ← return $ undefined dump
   hello
